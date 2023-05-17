e87d2f5a0f611e99330f3f2f4f509c8513863f70
Sourabh Patel <sourabhpatel073@gmail.com>
2023-05-17 13:48:15 +0530

    my first Python code

---
+++ PythonPractice/MyFirst.py
@@ -0,0 +1,20 @@
+print("Hello, World!")
+
+a = 10
+b = 5
+
+# Addition
+c = a + b  # c will be 15
+print(c)
+# Subtraction
+d = a - b  # d will be 5
+print(d)
+# Multiplication
+e = a * b  # e will be 50
+print(e)
+# Division
+f = a / b  # f will be 2.0 (float division)
+print(f)
+# Integer Division
+g = a // b  # g will be 2 (integer division)
+print(g)
