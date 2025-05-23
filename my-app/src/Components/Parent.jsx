0ef3439afc2a0394ae2e8db728046947e749e9dc
Sourabh Patel <sourabhpatel073@gmail.com>
2025-05-23 22:49:50 +0530

    practice

--- my-app/src/Components/Parent.jsx
+++ my-app/src/Components/Parent.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Child from './Child'
 
+
+
+function Display({screen}){
+return <p>{screen}</p>
+}
+
 function Parent() {
     const[count, setCount]=useState(0)
 
@@ -10,7 +16,7 @@ function Parent() {
   return (
     
     <div>
-      <Child count={count}></Child>
+      <Display screen={count}></Display>
       <button onClick={()=>{setCount(count+1)}}>add</button>
     </div>
    
